744ca08726c775bc8abb25c711f8fcbbe76d7798
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2019-11-05 21:28:10 +0530

    --Changed Shell Prompt --Added spring-shell.log to .gitignore

---
+++ src/main/java/iit/oop/cw/shell/CwPromptProvider.java
@@ -0,0 +1,17 @@
+package iit.oop.cw.shell;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CwPromptProvider implements PromptProvider {
+
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("OOP-CW:>",
+                AttributedStyle.DEFAULT.foreground(AttributedStyle.BLUE)
+        );
+    }
+}
