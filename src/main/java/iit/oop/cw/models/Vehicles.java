f7fafbdf1294e216d1549f641dfd951ecee68363
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-11-06 14:26:50 +0530

    --Finished Part 1 Tutorial --Added Sampled MongoDB Models and Repositories (Commented)

---
+++ src/main/java/iit/oop/cw/models/Vehicles.java
@@ -0,0 +1,45 @@
+//package iit.oop.cw.models;
+//
+//import org.springframework.data.annotation.Id;
+//
+//public class Vehicles {
+//
+//    @Id
+//    private String id;
+//
+//    private String plateNumber;
+//    private String make;
+//
+//    public Vehicles(){}
+//
+//    public Vehicles(String id, String plateNumber, String make) {
+//        this.id = id;
+//        this.plateNumber = plateNumber;
+//        this.make = make;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getPlateNumber() {
+//        return plateNumber;
+//    }
+//
+//    public void setPlateNumber(String plateNumber) {
+//        this.plateNumber = plateNumber;
+//    }
+//
+//    public String getMake() {
+//        return make;
+//    }
+//
+//    public void setMake(String make) {
+//        this.make = make;
+//    }
+//
+//}
---
+++ src/main/java/iit/oop/cw/repositories/VehicleRepository.java
@@ -0,0 +1,8 @@
+//package iit.oop.cw.repositories;
+//
+//import iit.oop.cw.models.Vehicles;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//
+//public interface VehicleRepository extends MongoRepository<Vehicles, String> {
+//    Vehicles findById(String id);
+//}
